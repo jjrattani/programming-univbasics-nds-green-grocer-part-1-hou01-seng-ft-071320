3caea9d393e266008c67bdaff02b5b1e18c6dba1
Jessica  Rattani <jjrattani@gmail.com>
2020-07-07 01:47:00 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -3,7 +3,7 @@ def find_item_by_name_in_collection(name, collection)
   #
   # Consult README for inputs and outputs
   collection.each do |grocery_item|
-    if grocery_item[item] == name
+    if grocery_item[:item] == name
       return item
     else
       return nil
