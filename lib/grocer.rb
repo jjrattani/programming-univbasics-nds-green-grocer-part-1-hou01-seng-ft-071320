1738256fafafe95b3823e79806cbb4048f0a9b01
Jessica  Rattani <jjrattani@gmail.com>
2020-07-07 01:56:01 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -16,6 +16,14 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  tallied_groceries = []
+  cart.each do |grocery_item|
+    # Check to see if the item already exists in the tallied_groceries
+    found = find_item_by_name_in_collection(grocery_item[:item], tallied_groceries)
+    if found
+      tallied_groceries.each do |tallied_item|
+        if tallied_item[:item] == grocery_item[:item]
+          
   puts cart
 end
 
